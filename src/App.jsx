3001e1f3c6992addab686b5f655123110ab09d6a
Isaac Galvan <isaacgdev@gmail.com>
2025-10-30 16:45:06 -0600

    adding calendar widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import KoalendarWidget from "./components/calendar.jsx";
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <KoalendarWidget />
     </>
   )
 }
